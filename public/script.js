72f518147f065e87a596b8ca385f8c224bcdc6e9
Uday Bansal <43824981+udaybansal19@users.noreply.github.com>
2020-01-03 16:52:39 +0530

    broken page

--- public/script.js
+++ public/script.js
@@ -1,8 +1,19 @@
-var socket = io.connect("http://192.168.137.1:8081/");
+var socket = io.connect("http://192.168.43.137:8081/");
 
 var output = document.getElementById("output");
 var message = document.getElementById("message");
 var send = document.getElementById("send");
+var constraints = { video : true };
+
+var peer = new Peer({key: 'lwjd5qra8257b9'});
+
+peer.on('open', function(id) {
+    console.log('My peer ID is: ' + id);
+  });
+
+  var conn = peer.connect('dest-peer-id');
+
+  
 
 send.addEventListener("click", ()=>{
     socket.emit("msg",message.value);
@@ -12,3 +23,13 @@ send.addEventListener("click", ()=>{
 socket.on("msg", (data) => {
     output.innerHTML += "<p>" + data + "</p>";
 });
+
+// function successCallback(stream){
+//     var video = document.getElementById("webcam");
+//     video.srcObject = stream;
+// }
+
+// function errorCallback(error) {
+//     console.log("navigator.getUserMedia: ",error);
+// }    
+// navigator.getUserMedia(constraints,successCallback,errorCallback);
