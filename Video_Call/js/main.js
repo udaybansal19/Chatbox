5fd4a0398cc940b4e974d8a1cb1e9910b7fc9ad8
Uday Bansal <43824981+udaybansal19@users.noreply.github.com>
2020-05-17 00:18:41 +0530

    Websocket url change in video_call

--- Video_Call/js/main.js
+++ Video_Call/js/main.js
@@ -24,7 +24,7 @@ const offerOptions = {
 var localStream;
 var remoteStream = new MediaStream();
 
-var socket = io.connect("http://192.168.1.16:80/",{secure : true});
+var socket = io.connect("http://3.17.66.109:80/",{secure : true});
 
 
   //Setting local stream.
